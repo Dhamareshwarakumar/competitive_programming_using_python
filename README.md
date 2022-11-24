1e0e30b2ee793e8ffff519365e7fd6a5a4901d60
Dhamareshwarakumar <damareshwarakumar@gmail.com>
2022-11-24 18:31:38 +0530

    final

---
+++ README.md
@@ -0,0 +1,15 @@
+# Competitive Programming Using Python
+
+## Description
+
+-   Info of the event Competitive Programming Using Python
+
+## Techstack
+
+-   Frontend: Basic HTML, CSS, JavaScript
+
+## Download & Run
+
+-   `git clone https://github.com/Dhamareshwarakumar/competitive_programming_using_python.git`
+-   `cd competitive_programming_using_python`
+-   `open index.html`
---
+++ script.js
@@ -0,0 +1,40 @@
+// Gallery
+(function($) {
+    $.fn.tilt = function() {
+        var perspective = '500px',
+            delta = 20,
+            width = this.width(),
+            height = this.height(),
+            midWidth = width / 2,
+            midHeight = height / 2;
+        this.on({
+            mousemove: function(e) {
+                var pos = $(this).offset(),
+                    cursPosX = e.pageX - pos.left,
+                    cursPosY = e.pageY - pos.top,
+                    cursCenterX = midWidth - cursPosX,
+                    cursCenterY = midHeight - cursPosY;
+
+                $(this).css('transform', 'perspective(' + perspective + ') rotateX(' + (cursCenterY / delta) + 'deg) rotateY(' + -(cursCenterX / delta) + 'deg)');
+                $(this).removeClass('is-out');
+            },
+            mouseleave: function() {
+                $(this).addClass('is-out');
+            }
+        });
+        //Return
+        return this;
+    };
+}(jQuery));
+
+//Set plugin on cards
+$('.card-image').tilt();
+
+
+
+// Winners
+$(".hover").mouseleave(
+    function() {
+      $(this).removeClass("hover");
+    }
+  );
